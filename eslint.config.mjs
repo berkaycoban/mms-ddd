ecd0352154aca1d73ccb4f310b824200d28a405c
Berkay COBAN <bcobann35@gmail.com>
2025-03-01 23:16:40 +0300

    feat: add eslint-import plugin for order imports and @ alias

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
+
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+
+// @ts-expect-error No types available
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   {
@@ -26,10 +30,58 @@ export default tseslint.config(
     },
   },
   {
+    plugins: {
+      import: importPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: import.meta.dirname + '/tsconfig.json',
+        },
+      },
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+
+      // turn on errors for missing imports
+      'import/no-unresolved': [
+        'error',
+        {
+          ignore: ['supertest'],
+        },
+      ],
+
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // Built-in imports (come from NodeJS native) go first
+            'external', // <- External imports
+            'internal', // <- Absolute imports
+            ['sibling', 'parent'], // <- Relative imports, the sibling and parent types they can be mingled together
+            'index', // <- index imports
+            'unknown', // <- unknown
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 );
