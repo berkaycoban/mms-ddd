13cbc8dbca7fe9b9020d063bbbb9b8398bc8e067
Berkay COBAN <bcobann35@gmail.com>
2025-03-01 23:24:32 +0300

    feat: implement clustering, add swagger and remove unused app files

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,74 @@
+import cluster from 'node:cluster';
+import { availableParallelism } from 'node:os';
+import process from 'node:process';
+
+import { type INestApplication, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  type SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { IS_PROD, PORT } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+  app.enableCors();
+
+  initSwagger(app);
+
+  await app.listen(PORT);
 }
-bootstrap();
+
+function initSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Movie Management System API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  const options: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+      deepLinking: true,
+      docExpansion: 'none',
+      tagsSorter: 'alpha',
+      filter: true,
+    },
+  };
+
+  SwaggerModule.setup('api-docs', app, document, options);
+}
+
+async function start() {
+  if (!IS_PROD) {
+    await bootstrap();
+    return;
+  }
+
+  const numCPUs = availableParallelism();
+
+  if (cluster.isPrimary) {
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', (worker) => {
+      console.log(`Worker ${worker.process.pid} died`);
+    });
+
+    return;
+  }
+
+  console.log(`Worker ${process.pid} started`);
+
+  await bootstrap();
+}
+
+void start();
